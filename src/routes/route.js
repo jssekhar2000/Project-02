c757bb04e4cba2385a70501a27562d452d06608c
jssekhar2000 <jssekhar2000@gmail.comm>
2022-07-31 13:16:36 +0530

    route

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,7 @@ const internsController = require("../Controller/internsController")
 
 
 
-router.post("/functisonup/colleges",collegeController.createCollege)
+router.post("/functionup/colleges",collegeController.createCollege)
 router.post("/functionup/interns",internsController.createInterns)
 router.get("/functionup/collegeDetails",internsController.getCollegeDetails)
 
